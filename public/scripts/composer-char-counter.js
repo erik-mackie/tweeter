19607924140728f350953e4be5193cc9cf4502fe
erik-mackie <erik.mackie@gmail.com>
2018-09-11 19:19:23 +0000

    pre ctrl Z

--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -1,14 +1,18 @@
 // count characters in text area and update display counter
 $(function() {
-  $(".textContainer textarea").keyup(function() {
+  $(".textContainer textarea").on("input", function() {
     let charCount = this.value.length ;
     /*$('.counter').text(140 - charCount);*/
     $(this).parent().siblings('span').text(140 - charCount);
     if (charCount > 140) {
        $(this).parent().siblings('span').css('color', 'red');
+    } else {
+      $(this).parent().siblings('span').css('color', 'black');
     }
   });
 });
+
+
 
 
 //maxlength attribute in textarea form?
