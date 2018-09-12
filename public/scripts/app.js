e894692b9f6fa386795d0be626326d1a48607fd5
erik-mackie <erik.mackie@gmail.com>
2018-09-12 23:35:54 +0000

    form toggle

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -3,37 +3,6 @@
  * jQuery is already loaded
  * Reminder: Use (and do all your DOM work in) jQuery's document ready function
  */
-/*var tweetData = [
-{
-    "user": {
-      "name": "Newton",
-      "avatars": {
-        "small":   "https://vanillicon.com/788e533873e80d2002fa14e1412b4188_50.png",
-        "regular": "https://vanillicon.com/788e533873e80d2002fa14e1412b4188.png",
-        "large":   "https://vanillicon.com/788e533873e80d2002fa14e1412b4188_200.png"
-      },
-      "handle": "@SirIsaac"
-    },
-    "content": {
-      "text": "If I have seen further it is by standing on the shoulders of giants"
-    },
-    "created_at": 1461116232227
-  },
-  {
-    "user": {
-      "name": "Descartes",
-      "avatars": {
-        "small":   "https://vanillicon.com/7b89b0d8280b93e2ba68841436c0bebc_50.png",
-        "regular": "https://vanillicon.com/7b89b0d8280b93e2ba68841436c0bebc.png",
-        "large":   "https://vanillicon.com/7b89b0d8280b93e2ba68841436c0bebc_200.png"
-      },
-      "handle": "@rd" },
-    "content": {
-      "text": "Je pense , donc je suis"
-    },
-    "created_at": 1461113959088
-  }
-];*/
 
 $(function() {
 
@@ -115,7 +84,7 @@ $(function() {
     })
   }
 
-
+// dont have side effects/ refactor
   function validate (tweet) {
     if (!tweet.val()) {
       alert("Please chirp before submitting");
@@ -145,6 +114,14 @@ $(function() {
       })
     }
   });
+
+  // on compose click toggle form visibility
+  $('#nav-bar button').click(function() {
+    // refactor DOM traversals in lets
+    let tw
+    $('.new-tweet').slideToggle("fast")
+    $('.new-tweet textarea').focus()
+  })
 
 });
 
