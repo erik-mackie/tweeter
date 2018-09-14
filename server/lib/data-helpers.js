3041b435f83e89823db4cb1eccd99140d8391902
erik-mackie <erik.mackie@gmail.com>
2018-09-14 00:27:06 +0000

    data helpers

--- server/lib/data-helpers.js
+++ server/lib/data-helpers.js
@@ -1,30 +1,25 @@
 "use strict";
 
 // Simulates the kind of delay we see with network or filesystem operations
+const MONGODB_URI = "mongodb://localhost:27017/tweeter";
 const simulateDelay = require("./util/simulate-delay");
-
 // Defines helper functions for saving and getting tweets,
 //using the database `db`
-module.exports = function makeDataHelpers(db) {
+module.exports = function makeDataHelpers(tweetDatabase) {
+
   return {
 
     // Saves a tweet to `db`
     saveTweet: function(newTweet, callback) {
-      simulateDelay(() => {
-        db.tweets.push(newTweet);
+        tweetDatabase.collection("tweets").insertOne(newTweet);
+
         callback(null, true);
-      });
     },
 
     // Get all tweets in `db`, sorted by newest first
     getTweets: function(callback) {
-      simulateDelay(() => {
-        const sortNewestFirst = (a, b) => a.created_at - b.created_at;
-        callback(null, db.tweets.sort(sortNewestFirst));
-      });
+      tweetDatabase.collection("tweets").find().toArray(callback);
     }
 
   };
 }
-
-
